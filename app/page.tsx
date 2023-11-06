27b78d52235c669aae408f69f6696e43079f986f
Aleksander Pankow <hello@pankow.me>
2023-11-06 17:42:24 +0100

    Home page: Mobile: max MD: FIX

--- app/page.tsx
+++ app/page.tsx
@@ -8,7 +8,7 @@ import Social from "@/components/Home/Social";
 
 export default function Home() {
     return (
-        <section className="">
+        <section className="container mx-auto">
             <Slider/>
             <Ticker/>
             <Events/>
