e988268d8e63578b77d7733f90827607db4af3bb
Aleksander Pankow <hello@pankow.me>
2023-11-10 09:03:25 +0100

    Mobile fixes

--- components/Home/Slider.tsx
+++ components/Home/Slider.tsx
@@ -2,8 +2,15 @@
 import Image from 'next/image';
 export default function Slider(){
     return(
-        <section className="h-[500px]">
-            <img src={"/images/mesto-vstrechi.png"} alt={""} className={"h-full"} />
+        <section className="h-3/5">
+            <Image
+                src="/images/mesto-vstrechi.png"
+                alt="My Image"
+                width={100}
+                height={100}
+                layout="responsive"
+                objectFit="cover"
+            />
         </section>
     )
 }
